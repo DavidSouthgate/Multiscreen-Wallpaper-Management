f6dca4d74da700b7007986dec00dea5b27cba0b3
David Southgate <dav@davidsouthgate.co.uk>
2015-05-25 14:21:02 +0100

    Initial2

--- MultiScreenWallpaper/Form1.Designer.cs
+++ MultiScreenWallpaper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MultiScreenWallpaper
 {
-    partial class Form1
+    partial class frmWallpaperManagement
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,15 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // frmWallpaperManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(287, 81);
+            this.Name = "frmWallpaperManagement";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Wallpaper Management";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- MultiScreenWallpaper/Program.cs
+++ MultiScreenWallpaper/Program.cs
@@ -16,7 +16,7 @@ namespace MultiScreenWallpaper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmWallpaperManagement());
         }
     }
 }
